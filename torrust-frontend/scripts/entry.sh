fdd13a6afc9b9e4516feaa9436be6b6d168e3095
Benjamin Arntzen <zorlin@gmail.com>
2022-02-22 04:28:47 +0800

    First working version

--- torrust-frontend/scripts/entry.sh
+++ torrust-frontend/scripts/entry.sh
@@ -1,18 +1,9 @@
 #!/bin/bash
 
-#if [ $1 == "create-config" ]
-#then
-#	echo "Creating configuration file."
-#	/opt/torrust/torrust-tracker/target/release/torrust-tracker
-#        cat config.toml
-#fi
-
-if [ ! -f "/opt/torrust/torrust-tracker/config.toml" ]; then
-	# Create configuration file by calling torrust-tracker, but don't collect any logging output.
-	/opt/torrust/torrust-tracker/target/release/torrust-tracker &> /dev/null
-	# Print the config file.
-	cat config.toml
-else
-	# Run torrust-tracker
-	/opt/torrust/torrust-tracker/target/release/torrust-tracker
+if [ $1 == "build" ]; then
+	cd /opt/torrust/torrust/frontend
+	npm i
+	npm run build
+	echo == DEBUG OUTPUT ==
+        cat /opt/torrust/torrust/frontend/.env
 fi
