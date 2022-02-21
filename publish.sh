3b4dc508ebeeb82dca8fe5400efe9f9e14721c31
Benjamin Arntzen <zorlin@gmail.com>
2022-02-22 04:32:47 +0800

    New publish script to publish dual-arch all containers

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
-# docker buildx build --platform linux/amd64,linux/arm64 -t zorlin/torrust-tracker:latest --push torrust-tracker
-#docker buildx build --platform linux/arm64 -t zorlin/torrust-tracker:latest --push torrust-tracker
-docker buildx build --platform linux/arm64 -t zorlin/torrust-backend:latest --push torrust-backend
+docker buildx build --platform linux/amd64,linux/arm64 -t zorlin/torrust-tracker:latest --push torrust-tracker
+docker buildx build --platform linux/amd64,linux/arm64 -t zorlin/torrust-frontend:latest --push torrust-frontend
+docker buildx build --platform linux/amd64,linux/arm64 -t zorlin/torrust-backend:latest --push torrust-backend
