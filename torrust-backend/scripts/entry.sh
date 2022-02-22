4a481dae566ab9045eb0b1191ca11d494d439ebb
Benjamin Arntzen <zorlin@gmail.com>
2022-02-22 16:02:18 +0100

    Fix for torrent uploads

--- torrust-backend/scripts/entry.sh
+++ torrust-backend/scripts/entry.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-if [ ! -f "/opt/torrust/torrust/backend/config.toml" ]; then
+if [ ! -f "/opt/torrust/torrust/backend/db/config.toml" ]; then
 	# Create configuration file by calling torrust-tracker, but don't collect any logging output.
 	/opt/torrust/torrust/backend/target/release/torrust &> /dev/null
 	# Print the config file.
 	cat config.toml
 else
+	echo '/tmp/core.%h.%e.%t' > /proc/sys/kernel/core_pattern
+	ulimit -c unlimited
 	# Change directory
         cd /opt/torrust/torrust/backend/db
 	ls /opt/torrust/torrust/backend/target/release/torrust
